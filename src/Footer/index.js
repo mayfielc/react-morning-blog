fb4c94d155f904c8e9f9c17f2d77f24d8823cbb9
mayfielc <christopher.mayfield72@gmail.com>
2019-09-24 10:42:10 -0500

    footer created

---
+++ src/Footer/index.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class Footer extends Component {
+    render() {
+        return (
+            <h5 className="footer">I am a gooter</h5>
+        )
+    }
+}
+
+export default Footer
